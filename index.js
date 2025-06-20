e3f7f2e2a318e6be3a1dd280d4896a3cd40329a1
Poojitha5j3 <poojithasamminga5j3@gmail.com>
2025-06-20 21:33:38 +0530

    Changed in Index.js for CI,CD

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const firmRoutes=require('./routes/firmRoutes');
 const productRoutes=require('./routes/productRoutes');
 const path=require('path')
 const app=express()
-const PORT=4000;
+const PORT=process.env.PORT||4000;
 dotEnv.config();
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDb connected succesfully!"))
@@ -20,6 +20,6 @@ app.use('/uploads',express.static('uploads'));
 app.listen(PORT,()=>{
     console.log(`server started and running at ${PORT}`);
 })
-app.use('/home',(req,res)=>{
+app.use('/',(req,res)=>{
     res.send("<h1>Welcome to HomeBite");
 })
